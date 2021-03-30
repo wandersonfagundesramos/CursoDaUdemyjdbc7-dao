56fa24993d8fd429a4463a059d4f45530637cb31
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2021-03-30 11:07:54 -0300

    Classe Seller (Vendedores)

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,22 @@
 package aplicacao;
 
+import java.util.Date;
+
 import modelo.entidades.Department;
+import modelo.entidades.Seller;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Programa em Java que acessa os dados do banco de dados MySQL.");
+		
+		System.out.println();
+		
 		Department obj = new Department(1, "Livros");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 }
